502625f12263ca2663f4de61831462e81d0b42c8
Nguyen Danh Khoi <khoi.nguyen@zalora.com>
2022-12-07 12:32:22 +0700

    fix wrong syntax

--- py/yamlGen.py
+++ py/yamlGen.py
@@ -14,7 +14,7 @@ for i in range(1,number_node+1):
     node['hostname']='node_'+str(i)
     node['image']='ethereum/client-go'
     node['command']='--nousb --datadir  /root/data --nodiscover --syncmode full --nodekey /root/files/priv.key --port '+str(http_port)+' --http --http.addr "0.0.0.0" --http.corsdomain="*"  --http.port '+str(port)+' --http.api db,eth,net,web3,admin,personal,miner,signer:insecure_unlock_protect  --networkid 1234 --unlock 0 --password /root/files/password --mine --allow-insecure-unlock'
-    volumes=[
+    volumes=[]
     volumes.append('./nodes/node_'+str(i)+'/keys/password:/root/files/password:ro')
     volumes.append('./nodes/node_'+str(i)+'/keys/priv.key:/root/files/priv.key:ro')
     volumes.append('./nodes/node_'+str(i)+'/data:/root/data:rw')
